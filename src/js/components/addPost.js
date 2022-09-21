3f5edd169a57be484f4c25e645ffecc5b491a652
Melisa-Zorraindo <melisa.zorraindo@gmail.com>
2022-09-21 17:43:46 +0200

    mobile - form to post content

--- src/js/components/addPost.js
+++ src/js/components/addPost.js
@@ -7,44 +7,37 @@ export function displayAddPostWindow() {
   MAIN.innerHTML = " ";
 
   //render post window
-  POST_WINDOW.innerHTML = `
-    <div class="card my-5 border-0">
-      <form action="" class="p-3">
-        <div class="form-floating">
-          <textarea
-            name=""
-            id="user-post-mobile"
-            class="form-control w-100 h-100"
-            placeholder="share your thoughts"
-            minlength="10"
-            required
-          ></textarea>
-          <label for="user-post">share your thoughts</label>
-        </div>
-        <div class="d-flex mt-3 mb-1 justify-content-between">
-          <div class="d-flex">
-            <button type="button" class="btn-custom text-secondary">
-              <span class="material-symbols-outlined">
-                add_photo_alternate
-              </span>
-            </button>
-            <button
-              type="button"
-              class="btn btn-custom text-secondary"
-            >
-              <span class="material-symbols-outlined"> add_reaction </span>
-            </button>
-          </div>
-          <div>
-            <button type="submit" class="btn btn-outline-primary">
-              close
-            </button>
-            <button type="submit" class="btn btn-primary">post</button>
-          </div>
-        </div>
-      </form>
-    </div>
-  `;
-
+  POST_WINDOW.innerHTML = `<div class="card my-1">
+                                <form action="" class="p-2">
+                                    <div class="form-floating">
+                                        <textarea
+                                          name=""
+                                          id="user-post-mobile"
+                                          class="form-control w-100 h-100"
+                                          placeholder="share your thoughts"
+                                          minlength="10"
+                                          required
+                                        ></textarea>
+                                        <label for="user-post">share your thoughts</label>
+                                    </div>
+                                    <div class="container">
+                                        <div class="row mt-3 mb-1">
+                                            <div class="col col-2">
+                                                <button type="button" class="btn btn-custom text-secondary">
+                                                    <span class="material-symbols-outlined">
+                                                      add_photo_alternate
+                                                    </span>
+                                                </button>
+                                            </div>
+                                            <div class="col text-end">
+                                                <button type="submit" class="btn btn-outline-primary">
+                                                  close
+                                                </button>
+                                                <button type="submit" class="btn btn-primary">post</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>`;
   MAIN.append(POST_WINDOW);
 }
