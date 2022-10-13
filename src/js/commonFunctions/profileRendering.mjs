aca6ed2d876001476774b86f45dd90f54472d942
Melisa-Zorraindo <melisa.zorraindo@gmail.com>
2022-10-14 01:50:33 +0200

    destructure user info

--- src/js/commonFunctions/profileRendering.mjs
+++ src/js/commonFunctions/profileRendering.mjs
@@ -7,7 +7,7 @@ import { getRandomImage } from "../tools/imagePicker.mjs";
  * @param {HTMLDivElement} container
  */
 export default async function renderUsers(arr, container) {
-  arr.forEach((user) => {
+  arr.forEach(({ avatar, name }) => {
     const li = document.createElement("li");
     li.classList.add("row", "py-2", "align-items-center");
     container.append(li);
@@ -18,19 +18,19 @@ export default async function renderUsers(arr, container) {
 
     //assign profile picture if user doesn't have one
     let assignedAvatar = getRandomImage();
-    if (user.avatar.length === 0) {
-      user.avatar = assignedAvatar;
+    if (avatar.length === 0) {
+      avatar = assignedAvatar;
     }
 
     const profilePicture = document.createElement("img");
-    profilePicture.setAttribute("src", user.avatar);
+    profilePicture.setAttribute("src", avatar);
     profilePicture.setAttribute("alt", "user profile picture");
-    profilePicture.classList.add("profile-pic", "rounded-circle");
+    profilePicture.classList.add("img-fluid", "profile-pic", "rounded-circle");
     imageContainer.append(profilePicture);
 
     const userName = document.createElement("div");
-    userName.classList.add("col-6");
-    userName.innerHTML = user.name;
+    userName.classList.add("col-6", "text-small");
+    userName.innerHTML = name;
     li.append(userName);
   });
 }
