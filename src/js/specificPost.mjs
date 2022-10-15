8f1664698a7a0cefe66abf59603c6ce0c5c02e16
Melisa-Zorraindo <melisa.zorraindo@gmail.com>
2022-10-15 14:51:10 +0200

    follow/unfollow specific post page

--- src/js/specificPost.mjs
+++ src/js/specificPost.mjs
@@ -1,8 +1,10 @@
 import { renderSinglePostHeader } from "./components/singlePostHeader.mjs";
 import { viewSpecificPost } from "./commonFunctions/api/posts/read.mjs";
 import { accessToken } from "./constants/storedKeys.mjs";
+import * as profiles from "./commonFunctions/api/profiles/read.mjs";
 import { Post } from "./components/post.mjs";
 import { getRandomImage } from "./tools/imagePicker.mjs";
+import renderUsers from "./commonFunctions/profileRendering.mjs";
 import logout from "./commonFunctions/logout.mjs";
 
 //get string param to display single post
@@ -57,6 +59,31 @@ newTitle.innerHTML = `Socials | Post by ${postItem.username}`;
 //render header
 const header = document.querySelector("#header");
 renderSinglePostHeader(header, avatar, name);
+
+//fetch information to display followers
+const users = await profiles.fetchProfiles();
+
+const signedInUser = localStorage.getItem("username");
+const userInformation = await profiles.viewProfile(accessToken, signedInUser);
+
+//render contacts dynamically
+const SOCIALS_USERS_CONTAINER = document.querySelector(
+  "#socials-users-container"
+);
+
+await renderUsers(users, SOCIALS_USERS_CONTAINER);
+
+//render followers dynamically
+const FOLLOWERS_CONTAINER = document.querySelector("#followers-container");
+const { followers } = userInformation;
+
+await renderUsers(followers, FOLLOWERS_CONTAINER);
+
+//render following dynamically
+const FOLLOWING_CONTAINER = document.querySelector("#following-container");
+const { following } = userInformation;
+
+await renderUsers(following, FOLLOWING_CONTAINER);
 
 //logout functionality for desktop
 const logoutButton = document.querySelector("#logout");
